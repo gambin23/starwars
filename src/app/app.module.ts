2da3734868d61f90715aead83f5974dd738a550e
Gilbert Gambin <gilbertgambin@gmail.com>
2018-12-22 08:55:58 +0100

    [CODE] Added initial planet search layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
 import { NavigationComponent } from './navigation/navigation.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PlanetDetailsComponent } from './planet-details/planet-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    PlanetDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatToolbarModule,
     LayoutModule,
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
     BrowserAnimationsModule
   ],
   providers: [],
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,20 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, startWith, debounceTime } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
+import { Planet } from 'src/models/Planet';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
+  planets: Planet[] = [{ name: 'Alderaan' }, { name: 'Yavin IV' }, { name: 'Dagobah' }, { name: 'Bespin' }, { name: 'Endor' },
+  { name: 'Naboo' }, { name: 'Coruscant' }, { name: 'Kamino' }, { name: 'Geonosis' }];
+  filteredPlanets: Observable<Planet[]>;
+  search = new FormControl();
+
+  ngOnInit() {
+    this.filteredPlanets = this.search.valueChanges.pipe(
+      debounceTime(1500),
+      startWith<string>(''),
+      map(value => this.filter(value)),
     );
+  }
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
+  private filter(value: string): Planet[] {
+    return this.planets.filter(planet => planet.name.toLowerCase().includes(value.toLowerCase())).sort();
+  }
 }
