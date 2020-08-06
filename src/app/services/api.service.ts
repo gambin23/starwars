c09593937fb378ffe122c6e6577e0f684dd181fe
Gilbert Gambin <gilbertgambin@gmail.com>
2020-08-06 21:01:44 +0200

    refactor(api): minor change

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Resident } from '../models/resident.model';
 import { PlanetList } from '../models/planet-list.model';
 
-const baseUrl = 'https://swapi.co/api/';
+const baseUrl = 'https://cors-anywhere.herokuapp.com/https://swapi.dev/api/';
 
 @Injectable({
   providedIn: 'root'
